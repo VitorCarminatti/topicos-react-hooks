534516f69a4b4e77178213a7f75de1af8d62b8d4
Vitor Carminatti <vitor.carminatti@hotmail.com>
2019-09-26 11:51:59 -0300

    Parte 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, Button, Card } from "antd";
-const { Meta } = Card;
+import { Form, Input, Button, Icon, List, Avatar, Modal } from "antd";
+import {
+  Container,
+  Header,
+  Body,
+  MovieList,
+  MovieItem,
+  MovieCard,
+  ListWrapper,
+  ModalContentWrapper,
+  Title,
+  ComparisonList
+} from "./styles";
+
+import _ from "lodash";
+
+const { Meta } = MovieCard;
+const noImageUrl = "https://static.thenounproject.com/png/340719-200.png";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  // const [likes, setLikes] = useState(0);
+  const [likedMovies, setLikedMovies] = useState([]);
   const [title, setTitle] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [movieToCompare, setMovieToCompare] = useState({});
+  const [error, setError] = useState("");
 
   const searchMovies = () => {
+    setError("");
     fetch(`http://www.omdbapi.com/?apikey=c5e2aa30&s=${title}`)
       .then(response => response.json())
-      .then(result => setMovies(result.Search))
-      .catch(e => setMovies([]));
+      .then(result => {
+        result.Response === "True"
+          ? setMovies(result.Search)
+          : setError(result.Error);
+      });
   };
 
   const handleSubmit = e => {
@@ -20,52 +44,195 @@ function App() {
     searchMovies();
   };
 
-  // const handleClick = id => {
-  //   const novosPosts = posts.map(post => {
-  //     return post.id === id ? { ...post, like: !post.like } : post;
-  //   });
+  const handleClick = id => {
+    const newMovies = movies.map(movie => {
+      return movie.imdbID === id ? { ...movie, like: !movie.like } : movie;
+    });
 
-  //   setPosts(novosPosts);
-  // };
+    setMovies(newMovies);
+  };
 
-  // useEffect(() => {
-  //   const curtidas = posts.filter(post => post.like).length;
-  //   setLikes(curtidas);
-  // }, [posts]);
+  const handleOk = () => {
+    setMovieToCompare({});
+    setVisible(false);
+  };
+  const handleCancel = () => {
+    setMovieToCompare({});
+    setVisible(false);
+  };
+
+  const openModal = id => {
+    const movie = movies.find(movie => movie.imdbID === id);
+    setSelectedMovie(movie);
+    setVisible(true);
+  };
+
+  const setComparison = id => {
+    const movie = movies.find(movie => movie.imdbID === id);
+    setMovieToCompare(movie);
+  };
+
+  useEffect(() => {
+    const likedOnes = movies.filter(movie => movie.like);
+    setLikedMovies(likedOnes);
+  }, [movies]);
 
   return (
-    <>
-      <Form layout="inline" onSubmit={handleSubmit}>
-        <Form.Item>
-          <Input
-            onChange={e => setTitle(e.target.value)}
-            placeholder="Título do filme"
-            value={title}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Buscar
-          </Button>
-        </Form.Item>
-      </Form>
-
-      <ul>
-        {movies.length > 0 &&
-          movies.map(movie => {
-            console.log(movie);
-            return (
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="poster" src={movie.Poster} />}
-              >
-                <Meta title={movie.Title} description={movie.Year} />
-              </Card>
-            );
-          })}
-      </ul>
-    </>
+    <Body>
+      <Title>Search, Compare and Like movies</Title>
+      <Header>
+        <Form layout="inline" onSubmit={handleSubmit}>
+          <Form.Item>
+            <Input
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Movie Title"
+              value={title}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Buscar
+            </Button>
+          </Form.Item>
+        </Form>
+      </Header>
+      <Container>
+        <MovieList>
+          {error !== "" ? (
+            <h3>{error}</h3>
+          ) : (
+            movies.map(movie => {
+              return (
+                <MovieItem key={movie.imdbID}>
+                  <MovieCard
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={
+                      <img
+                        onClick={() => openModal(movie.imdbID)}
+                        alt="poster"
+                        src={movie.Poster === "N/A" ? noImageUrl : movie.Poster}
+                        height={300}
+                      />
+                    }
+                    actions={[
+                      <Icon
+                        onClick={() => handleClick(movie.imdbID)}
+                        theme={movie.like ? "filled" : "outlined"}
+                        type="like"
+                        key="like"
+                      />
+                    ]}
+                  >
+                    <Meta
+                      title={movie.Title}
+                      description={`${movie.Year} | ${movie.Type}`}
+                    />
+                  </MovieCard>
+                </MovieItem>
+              );
+            })
+          )}
+        </MovieList>
+        {likedMovies.length > 0 && (
+          <ListWrapper>
+            <h3>Liked Movies</h3>
+            <List
+              dataSource={likedMovies}
+              renderItem={item => (
+                <List.Item key={item.imdbID}>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        src={item.Poster === "N/A" ? noImageUrl : item.Poster}
+                      />
+                    }
+                    title={<p>{item.Title}</p>}
+                    description={item.Year}
+                  />
+                </List.Item>
+              )}
+            />
+          </ListWrapper>
+        )}
+      </Container>
+      <Modal
+        title="Compare movies"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <ModalContentWrapper>
+          <MovieCard
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="poster"
+                src={
+                  selectedMovie.Poster === "N/A"
+                    ? noImageUrl
+                    : selectedMovie.Poster
+                }
+                height={300}
+              />
+            }
+          >
+            <Meta
+              title={selectedMovie.Title}
+              description={`${selectedMovie.Year} | ${selectedMovie.Type}`}
+            />
+          </MovieCard>
+          {!!_.isEmpty(movieToCompare) ? (
+            <ListWrapper>
+              <h4>Select a movie to compare</h4>
+              <ComparisonList
+                dataSource={movies.filter(
+                  movie => movie.imdbID !== selectedMovie.imdbID
+                )}
+                renderItem={item => (
+                  <ComparisonList.Item
+                    onClick={() => setComparison(item.imdbID)}
+                    key={item.imdbID}
+                  >
+                    <ComparisonList.Item.Meta
+                      avatar={
+                        <Avatar
+                          src={item.Poster === "N/A" ? noImageUrl : item.Poster}
+                        />
+                      }
+                      title={<p>{item.Title}</p>}
+                      description={item.Year}
+                    />
+                  </ComparisonList.Item>
+                )}
+              />
+            </ListWrapper>
+          ) : (
+            <MovieCard
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="poster"
+                  src={
+                    movieToCompare.Poster === "N/A"
+                      ? noImageUrl
+                      : movieToCompare.Poster
+                  }
+                  height={300}
+                />
+              }
+            >
+              <Meta
+                title={movieToCompare.Title}
+                description={`${movieToCompare.Year} | ${movieToCompare.Type}`}
+              />
+            </MovieCard>
+          )}
+        </ModalContentWrapper>
+      </Modal>
+    </Body>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Card, List } from "antd";
+
+export const Body = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const MovieList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+export const MovieItem = styled.li`
+  list-style-type: none;
+  margin-top: 20px;
+`;
+
+export const MovieCard = styled(Card)``;
+
+export const ListWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+export const ModalContentWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const Title = styled.h1`
+  align-self: center;
+`;
+
+export const ComparisonList = styled(List)`
+  overflow: scroll;
+  max-height: 50vh;
+`;
